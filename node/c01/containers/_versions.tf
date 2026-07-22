c378b1f8eb531fb2d1e3f5cd997c079fc186dfd1
Matheus C.R. <matheus.carvalho@canonical.com>
2026-07-22 08:59:02 -0300

    feat (node/c01/containers): create wireguard config file from template

--- node/c01/containers/_versions.tf
+++ node/c01/containers/_versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 4.5.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.9.0"
+    }
   }
 }
--- node/c01/containers/wireguard-client.tf
+++ node/c01/containers/wireguard-client.tf
@@ -1,3 +1,30 @@
+variable "wireguard_client_keys" {
+  type = object({
+    private_key   = string
+    public_key    = string
+    preshared_key = string
+  })
+
+  sensitive = true
+
+  # Enforce non-null constraints using validation blocks
+  validation {
+    condition     = var.vpn_keys.private_key != null && var.vpn_keys.public_key != null && var.vpn_keys.preshared_key != null
+    error_message = "All keys (private_key, public_key, and preshared_key) must be non-null values."
+  }
+}
+
+resource "local_sensitive_file" "wireguard_client_conf_file" {
+  filename = abspath("${path.module}/configs/wireguard-client.wg.conf")
+
+  # Render the template and pass variables into it
+  content = templatefile(abspath("${path.module}/configs/wireguard-client_conf.tftpl"), {
+    private_key   = var.wireguard_client_keys.private_key
+    public_key    = var.wireguard_client_keys.public_key
+    preshared_key = var.wireguard_client_keys.preshared_key
+  })
+}
+
 resource "docker_image" "wireguard_client_image" {
   name          = "linuxserver/wireguard:latest"
   keep_locally  = false
@@ -18,8 +45,18 @@ resource "docker_container" "wireguard_client" {
     read_only      = true
   }
   volumes {
+    host_path      = abspath("${path.root}/configs/wireguard-client.conf")
+    container_path = "/config/wg_confs/wg0.conf"
+    read_only      = true
+  }
+  volumes {
     host_path      = "/lib/modules"
     container_path = "/lib/modules"
     read_only      = true
+  }
+  lifecycle {
+    replace_triggered_by = [
+      local_sensitive_file.wireguard_client_conf_file
+    ]
   }
 }
