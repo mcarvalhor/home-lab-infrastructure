74b28f9391b716e89ad4b87de3dcd52c51e08fcf
Matheus C.R. <matheus.carvalho@canonical.com>
2026-07-27 22:51:46 -0300

    fix (node/c01/containers/nginx_torrent-dl.tf): fix index charset

--- node/c01/containers/nginx_torrent-dl.tf
+++ node/c01/containers/nginx_torrent-dl.tf
@@ -9,16 +9,11 @@ resource "docker_container" "nginx_torrent_dl" {
   image                 = docker_image.nginx_torrent_dl_image.image_id
   restart               = "unless-stopped"
   destroy_grace_seconds = 30
+  command               = ["bash", "-c", "echo >> /usr/local/apache2/conf/httpd.conf && echo 'IndexOptions +Charset=UTF-8' >> /usr/local/apache2/conf/httpd.conf && httpd-foreground"]
   ports {
     internal = 80
     external = local.ports.nginx_torrent_dl
   }
-  env = [
-    "HTTPD_PREFIX=/usr/local/apache2",
-    "HTTPD_VERSION=2.4.57",
-    "HTTPD_SHA256=dbccb84aee95e095edfbb81e5eb926ccd24e6ada55dcd83caecb262e5cf94d2a",
-    "HTTPD_PATCHES=rewrite-windows-testchar-h.patch 1d5620574fa03b483262dc5b9a66a6906553389952ab5d3070a02f887cc20193",
-  ]
   volumes {
     host_path      = "/nas/smb/Torrents"
     container_path = "/usr/local/apache2/htdocs"
